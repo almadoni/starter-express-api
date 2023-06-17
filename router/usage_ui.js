eea1d46551f17b324596a7cef9ed03ca7937fb58
Almadoni <almadoni@l7systems.net>
2023-06-17 12:29:08 +0700

    add lagi

--- router/usage_ui.js
+++ router/usage_ui.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/list_usage', (req, res) =>{
 
-	pool.query("select a.*, to_char(create_date, 'Day, DD HH12:MI:SS') as created_date,b.fullname from usage_history a left join accounts b on (a.user_id = b.id) order by created_date desc", (error, results) =>{
+	pool.query("select a.*,b.fullname from usage_history a left join accounts b on (a.user_id = b.id) order by created_date desc", (error, results) =>{
           if(error){
              throw error
           }

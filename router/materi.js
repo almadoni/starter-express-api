797757134333a7a05e58a52e608046026fba6a56
Almadoni <almadoni@l7systems.net>
2023-06-19 17:02:57 +0700

    update lagi

--- router/materi.js
+++ router/materi.js
@@ -7,7 +7,7 @@ const getMateries = (req, res) => {
 
         id = req.params.id
 
-        pool.query('select c.id, c.fullname as assign_to,a.path, a.name,a.id as materi_id, a.create_date as create_materi from materi a left join materi_assign b on (a.id = b.materi_id) left join accounts c on (c.id = b.account_id) where c.id = $1',[id], (error, results) =>{
+        pool.query('select c.id, c.fullname as assign_to,a.path, a.name,a.id as materi_id, a.create_date as create_materi from materi a left join materi_assign b on (a.id = b.materi_id) left join accounts c on (c.id = b.account_id) where c.id = '+id, (error, results) =>{
           if(error){
 	     res.status(200).json({code: "9999", result: error})  
              throw error
